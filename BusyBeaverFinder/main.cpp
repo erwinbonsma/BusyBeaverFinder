08928af984bf77618409b2b3d6534eb5a93c21b4
Erwin Bonsma <eriban@users.sourceforge.net>
2019-01-19 10:10:59 +0100

    Initial Commit

---
+++ BusyBeaverFinder/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  BusyBeaverFinder
+//
+//  Created by Erwin on 19/01/19.
+//  Copyright © 2019 Erwin. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
