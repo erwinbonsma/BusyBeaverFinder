3f70ad36e31372623f2ac3841540ab1035c88915
Erwin Bonsma <eriban@users.sourceforge.net>
2019-02-13 20:33:06 +0100

    Extended SearchOrchestrator for second TURN on bottom row
    When the second TURN is on the bottom row, the order of DATA instructions before it also does not matter. So also here, only the canonical variants need to be searched.

--- BusyBeaverFinder/SearchOrchestrator.cpp
+++ BusyBeaverFinder/SearchOrchestrator.cpp
@@ -15,17 +15,31 @@ SearchOrchestrator::SearchOrchestrator(ExhaustiveSearcher& searcher)
     // void
 }
 
+Op* SearchOrchestrator::addOperationsUntilTurn(Op* topP, int numNoop, int numData) {
+    while (numNoop-- > 0) {
+        *(topP++) = Op::NOOP;
+    }
+    while (numData-- > 0) {
+        *(topP++) = Op::DATA;
+    }
+    *(topP++) = Op::TURN;
+
+    return topP;
+}
+
 void SearchOrchestrator::search() {
     int h = _searcher.getProgram().getHeight();
-    Op* startFrom = new Op[h + 1];
+    int w = _searcher.getProgram().getWidth();
+    int maxOps = (h + 1 > w + 2) ? h + 1 : w + 2;
+    Op* startFrom = new Op[maxOps];
 
     // Only the number of DATA instructions before the first TURN matters, not their position, as
     // they will only be visited once at the very start of the program (and possibly as final
     // instruction before termination). So there is no need to iterate over all possible
     // permutations of DATA and NOOP instructions. Therefore only the canonical variant is searched.
     //
-    // y = The position of first TURN instructions that is encountered.
-    // n = The number of DATA instructions encountered before the first TURN
+    // y = The (row) position of first TURN instructions that is encountered.
+    // n = The number of DATA instructions preceding the first TURN
     //
     // For the program to not terminate immediately, at least one DATA instruction should precede
     // the first TURN. Therefore, y > 0 and n > 0.
@@ -33,17 +47,30 @@ void SearchOrchestrator::search() {
         for (int n = 1; n <= y; n++ ) {
             int numData = n;
             int numNoop = y - numData;
-            int i = 0;
-            while (numNoop-- > 0) {
-                startFrom[i++] = Op::NOOP;
-            }
-            while (numData-- > 0) {
-                startFrom[i++] = Op::DATA;
-            }
-            startFrom[i++] = Op::TURN;
-            startFrom[i++] = Op::UNSET;
 
-            _searcher.searchSubTree(startFrom);
+            Op* topP = addOperationsUntilTurn(startFrom, numNoop, numData);
+
+            if (y == 1) {
+                // When the first TURN is at the second row, the program pointer traverses the first
+                // row. Here again, only the number of DATA instructions before the second TURN
+                // matters, not their order.
+                //
+                // x = The column position of the second TURN instruction
+                // m = The number of DATA instructions preceding this second TURN
+                for (int x = 2; x < w; x++) {
+                    for (int m = 1; m < x; m++) {
+                        numData = m;
+                        numNoop = x - numData - 1;
+                        Op* topP2 = addOperationsUntilTurn(topP, numNoop, numData);
+
+                        (*topP2) = Op::UNSET;
+                        _searcher.searchSubTree(startFrom);
+                    }
+                }
+            } else {
+                (*topP) = Op::UNSET;
+                _searcher.searchSubTree(startFrom);
+            }
         }
     }
 
--- BusyBeaverFinder/SearchOrchestrator.h
+++ BusyBeaverFinder/SearchOrchestrator.h
@@ -11,10 +11,17 @@
 
 #include <stdio.h>
 
+#include "Enums.h"
+
 class ExhaustiveSearcher;
 
 class SearchOrchestrator {
     ExhaustiveSearcher& _searcher;
+
+    // Adds operations to the operation stack where topP points to the top of the stack. It first
+    // adds numNoop NOOP instructions, then numData DATA instructions followed by one turn
+    // instruction. It returns the pointer to the new top of the stack.
+    Op* addOperationsUntilTurn(Op* topP, int numNoop, int numData);
 
 public:
     SearchOrchestrator(ExhaustiveSearcher& searcher);
