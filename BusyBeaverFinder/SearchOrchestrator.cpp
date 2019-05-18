4cfc80cbaba70ef7ee3d5232423dcaa7d0ec3d95
Erwin Bonsma <eriban@users.sourceforge.net>
2019-05-19 00:12:19 +0200

    Changed iteration order of search orchestrator
    Changed iteration order so that the search space is visited in the same order as is done by the full recursive search.
    This improves consistency and also makes it easier to track the progress of the orchestrated search.

--- BusyBeaverFinder/SearchOrchestrator.cpp
+++ BusyBeaverFinder/SearchOrchestrator.cpp
@@ -38,30 +38,26 @@ void SearchOrchestrator::search() {
     // instruction before termination). So there is no need to iterate over all possible
     // permutations of DATA and NOOP instructions. Therefore only the canonical variant is searched.
     //
-    // y = The (row) position of first TURN instructions that is encountered.
-    // n = The number of DATA instructions preceding the first TURN
+    // numNoop = The number of NOOP instructions before the first TURN
+    // numData = The number of DATA instructions before the first TURN
     //
     // For the program to not terminate immediately, at least one DATA instruction should precede
-    // the first TURN. Therefore, y > 0 and n > 0.
-    for (int y = 1; y < h; y++) {
-        for (int n = 1; n <= y; n++ ) {
-            int numData = n;
-            int numNoop = y - numData;
-
+    // the first TURN. Therefore: numData > 0
+    for (int numNoop = h - 1; --numNoop >= 0; ) {
+        for (int numData = h - numNoop; --numData >= 1; ) {
             Ins* topP = addInstructionsUntilTurn(startFrom, numNoop, numData);
 
-            if (y == 1) {
+            if (numData == 1 && numNoop == 0) {
                 // When the first TURN is at the second row, the program pointer traverses the first
                 // row. Here again, only the number of DATA instructions before the second TURN
                 // matters, not their order.
                 //
-                // x = The column position of the second TURN instruction
-                // m = The number of DATA instructions preceding this second TURN
-                for (int x = 2; x < w; x++) {
-                    for (int m = 1; m < x; m++) {
-                        numData = m;
-                        numNoop = x - numData - 1;
-                        Ins* topP2 = addInstructionsUntilTurn(topP, numNoop, numData);
+                // numNoop2 = The number of NOOP instructions before the second TURN
+                // numData2 = The number of DATA instructions before the second TURN. It excludes
+                //            the single data instruction before the first TURN
+                for (int numNoop2 = w - 1; --numNoop2 >= 0; ) {
+                    for (int numData2 = w - numNoop2 - 1; --numData2 >= 1; ) {
+                        Ins* topP2 = addInstructionsUntilTurn(topP, numNoop2, numData2);
 
                         (*topP2) = Ins::UNSET;
                         _searcher.searchSubTree(startFrom);
