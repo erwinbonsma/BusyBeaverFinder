fbc4345d1b9c67bf9cdc46b636a759fa83b84d75
Erwin Bonsma <eriban@users.sourceforge.net>
2019-05-26 18:12:12 +0200

    Added a few more late-escape follow-up tests

--- Tests/LateEscapeFollowUpTests.cpp
+++ Tests/LateEscapeFollowUpTests.cpp
@@ -24,7 +24,47 @@ TEST_CASE( "7x7 Late Escape Follow-Up tests", "[7x7][late-escape]" ) {
     settings.maxSteps = 10000000;
     searcher.configure(settings);
 
+    SECTION( "EscapeIntoSuccess" ) {
+        // After escape terminates without encountering unset instructions
+        //
+        //   *     * *
+        // * o o _ _ _ *
+        //   _ * * o _
+        // * o _ o o *
+        // o _ o o o *
+        // o * _ _ o *
+        // o       *
+        Ins resumeFrom[] = {
+            Ins::DATA, Ins::DATA, Ins::DATA, Ins::TURN, Ins::NOOP, Ins::DATA, Ins::DATA, Ins::DATA,
+            Ins::TURN, Ins::DATA, Ins::DATA, Ins::NOOP, Ins::TURN, Ins::NOOP, Ins::TURN, Ins::NOOP,
+            Ins::TURN, Ins::TURN, Ins::DATA, Ins::TURN, Ins::NOOP, Ins::NOOP, Ins::TURN, Ins::NOOP,
+            Ins::TURN, Ins::DATA, Ins::TURN, Ins::DATA, Ins::NOOP, Ins::DATA, Ins::TURN, Ins::DATA,
+            Ins::NOOP, Ins::TURN, Ins::TURN, Ins::UNSET
+        };
+
+        searcher.searchSubTree(resumeFrom);
+
+        REQUIRE(tracker.getTotalSuccess() == 1);
+        REQUIRE(tracker.getMaxStepsFound() == 3152126);
+    }
     SECTION( "EscapeIntoSearch" ) {
+        // After escape encounters a single unset instructions, which results in three possible
+        // programs
+        Ins resumeFrom[] = {
+            Ins::DATA, Ins::TURN, Ins::NOOP, Ins::DATA, Ins::TURN, Ins::DATA, Ins::NOOP, Ins::DATA,
+            Ins::TURN, Ins::DATA, Ins::TURN, Ins::DATA, Ins::DATA, Ins::TURN, Ins::NOOP, Ins::TURN,
+            Ins::DATA, Ins::DATA, Ins::TURN, Ins::NOOP, Ins::TURN, Ins::NOOP, Ins::TURN, Ins::DATA,
+            Ins::DATA, Ins::TURN, Ins::NOOP, Ins::TURN, Ins::NOOP, Ins::NOOP, Ins::NOOP, Ins::DATA,
+            Ins::TURN, Ins::TURN, Ins::TURN, Ins::TURN, Ins::UNSET
+        };
+
+        searcher.searchSubTree(resumeFrom);
+
+        REQUIRE(tracker.getTotalHangs(HangType::NO_EXIT) == 1);
+        REQUIRE(tracker.getTotalSuccess() == 2);
+        REQUIRE(tracker.getMaxStepsFound() == 1648533);
+    }
+    SECTION( "EscapeIntoSearch2" ) {
         // Escapes after more than 3M steps. This can result in various programs: some that
         // complete and others that hang (in two types of detectable hang).
         //
@@ -44,8 +84,6 @@ TEST_CASE( "7x7 Late Escape Follow-Up tests", "[7x7][late-escape]" ) {
         };
 
         searcher.searchSubTree(resumeFrom);
-
-        tracker.dumpFinalStats();
 
         REQUIRE(tracker.getTotalHangs(HangType::NO_DATA_LOOP) == 1);
         REQUIRE(tracker.getTotalHangs(HangType::NO_EXIT) == 2);
