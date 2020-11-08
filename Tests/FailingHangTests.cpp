1da62c8a4a16c006b49a215ab9d6c419040c50b2
Erwin Bonsma <eriban@users.sourceforge.net>
2020-11-08 14:24:55 +0100

    Add more failing regular sweep hang tests

--- Tests/FailingHangTests.cpp
+++ Tests/FailingHangTests.cpp
@@ -22,10 +22,12 @@ TEST_CASE( "6x6 Failing Hang tests", "[hang][regular][sweep][6x6][.fail]" ) {
     settings.maxHangDetectAttempts = 1024;
     searcher.configure(settings);
 
-    SECTION( "6x6-EndlessMetaLoop") {
-        // Sweep that gets stuck in an endless meta-run loop. It basically alternates between two
-        // reversal sequences at the left end. It either turns at the end of the sequence, or just
-        // before it.
+    SECTION( "6x6-IrregularGrowthFailing") {
+        // Sweep with irregular growth at its left side. It alternates between two reversal
+        // sequences:
+        //
+        // "... 0 0 1 1 [body]" => "... 0 1 0 3 [body]"
+        // "... 0 0 1 0 [body]" => "... 0 0 1 1 [body]"
         //
         //     * *
         //   * o _ _ *
@@ -38,6 +40,72 @@ TEST_CASE( "6x6 Failing Hang tests", "[hang][regular][sweep][6x6][.fail]" ) {
             Ins::NOOP, Ins::DATA, Ins::TURN, Ins::NOOP, Ins::TURN, Ins::DATA, Ins::TURN, Ins::NOOP,
             Ins::TURN, Ins::DATA, Ins::NOOP, Ins::DATA, Ins::NOOP, Ins::TURN, Ins::DATA, Ins::TURN,
             Ins::TURN, Ins::UNSET
+        };
+        searcher.findOne(resumeFrom);
+
+        REQUIRE(tracker.getTotalDetectedHangs() == 1);
+    }
+    SECTION( "6x6-SteadyGrowthSweepWithInSequenceExit") {
+        // Dual-headed sweep with a complex transition at the right end. The right side of the
+        // sequence consists of two 1's. The left-most one terminates the sweep. It is then
+        // converted to a -1, extending the sequence body. Furthermore, it adds a new 1 value right
+        // of the other one.
+        //
+        //       *
+        //   * * o _ *
+        // * _ o o *
+        // * o _ o
+        // * * _ o
+        // o o o *
+        Ins resumeFrom[] = {
+            Ins::DATA, Ins::TURN, Ins::DATA, Ins::DATA, Ins::TURN, Ins::NOOP, Ins::NOOP, Ins::DATA,
+            Ins::TURN, Ins::DATA, Ins::TURN, Ins::DATA, Ins::TURN, Ins::NOOP, Ins::TURN, Ins::DATA,
+            Ins::DATA, Ins::TURN, Ins::NOOP, Ins::TURN, Ins::DATA, Ins::TURN, Ins::TURN, Ins::UNSET
+        };
+        searcher.findOne(resumeFrom);
+
+        REQUIRE(tracker.getTotalDetectedHangs() == 1);
+    }
+    SECTION( "6x6-SteadyGrowthSweepWithInSequenceExit2") {
+        // Similar in behaviour to previous program, but with a much more complicated transition
+        // sequence. The transition extends the sequence as follows:
+        // .. -1 1 2 =>
+        // .. -2 1 2 =>
+        // .. -2 0 2 =>
+        // .. -2 0 2 2 =>
+        // .. -2 -3 2 2 =>
+        // .. -2 -2 2 2 =>
+        // .. -2 -2 0 2 =>
+        // .. -2 -1 0 2 =>
+        // .. -2 -1 1 2
+        //
+        //       *
+        //     * o _ *
+        // * _ _ o *
+        //   _ o o *
+        // * * _ o _ *
+        // o _ o *
+        Ins resumeFrom[] = {
+            Ins::DATA, Ins::TURN, Ins::NOOP, Ins::DATA, Ins::TURN, Ins::NOOP, Ins::DATA, Ins::NOOP,
+            Ins::TURN, Ins::DATA, Ins::TURN, Ins::DATA, Ins::TURN, Ins::NOOP, Ins::TURN, Ins::DATA,
+            Ins::DATA, Ins::TURN, Ins::NOOP, Ins::TURN, Ins::NOOP, Ins::TURN, Ins::NOOP, Ins::TURN,
+            Ins::UNSET
+        };
+        searcher.findOne(resumeFrom);
+
+        REQUIRE(tracker.getTotalDetectedHangs() == 1);
+    }
+    SECTION( "6x6-SimpleSweepFailure") {
+        //       *
+        //   * * o _ *
+        //   _ o o *
+        //   _ _ o
+        // * _ _ o
+        // o o o *
+        Ins resumeFrom[] = {
+            Ins::DATA, Ins::TURN, Ins::DATA, Ins::DATA, Ins::TURN, Ins::NOOP, Ins::NOOP, Ins::DATA,
+            Ins::TURN, Ins::DATA, Ins::TURN, Ins::DATA, Ins::TURN, Ins::NOOP, Ins::TURN, Ins::DATA,
+            Ins::DATA, Ins::NOOP, Ins::NOOP, Ins::NOOP, Ins::TURN, Ins::UNSET
         };
         searcher.findOne(resumeFrom);
 
