37f68b3776c07665b645b49abb6486fdb523e80d
Erwin Bonsma <eriban@users.sourceforge.net>
2020-11-29 13:30:39 +0100

    Add a few more regular hangs that are not yet detected

--- Tests/FailingHangTests.cpp
+++ Tests/FailingHangTests.cpp
@@ -43,4 +43,87 @@ TEST_CASE( "6x6 Failing Hang tests", "[hang][regular][sweep][6x6][fail]" ) {
         // TEMP: Should not yet be detected with current logic. Eventually it should be detected.
         REQUIRE(tracker.getTotalDetectedHangs() == 0);
     }
+    SECTION( "6x6-SweepWithFixedPointMultiValueExitAndInSweepOscillatingChange2" ) {
+        // Similar in behavior to the previous program. Here, the sequence at the right either ends
+        // with 0 -1 or -1 -1.
+        //
+        //       * *
+        //   * * _ o *
+        // * o _ _ *
+        // o o o o *
+        // o * o _ *
+        // _   * *
+        Ins resumeFrom[] = {
+            Ins::NOOP, Ins::DATA, Ins::DATA, Ins::TURN, Ins::DATA, Ins::DATA, Ins::DATA, Ins::TURN,
+            Ins::NOOP, Ins::NOOP, Ins::TURN, Ins::TURN, Ins::NOOP, Ins::TURN, Ins::DATA, Ins::TURN,
+            Ins::TURN, Ins::TURN, Ins::DATA, Ins::TURN, Ins::TURN, Ins::TURN, Ins::NOOP, Ins::DATA,
+            Ins::TURN, Ins::UNSET
+        };
+        searcher.findOne(resumeFrom);
+
+        // TEMP: Should not yet be detected with current logic. Eventually it should be detected.
+        REQUIRE(tracker.getTotalDetectedHangs() == 0);
+    }
+    SECTION( "6x6-SweepHangWithMidSweepLoopSwitchViaTransitionWithLoop" ) {
+        // The rightwards sweep switches loops. Both loops are separated by a transition sequence
+        // which itself contains a short loop, with a fixed number of iterations.
+        //
+        //       *
+        // *   * o _ *
+        // o o o o *
+        // _ * _ o o *
+        // _   _ * *
+        // _   *
+        Ins resumeFrom[] = {
+            Ins::NOOP, Ins::NOOP, Ins::NOOP, Ins::DATA, Ins::TURN, Ins::DATA, Ins::DATA, Ins::DATA,
+            Ins::TURN, Ins::DATA, Ins::TURN, Ins::NOOP, Ins::TURN, Ins::TURN, Ins::DATA, Ins::TURN,
+            Ins::NOOP, Ins::TURN, Ins::DATA, Ins::TURN, Ins::NOOP, Ins::TURN, Ins::TURN, Ins::UNSET
+        };
+        searcher.findOne(resumeFrom);
+
+        // TEMP: Should not yet be detected with current logic. Eventually it should be detected.
+        REQUIRE(tracker.getTotalDetectedHangs() == 0);
+    }
+    SECTION( "6x6-UndetectedGlider" ) {
+        //       *
+        // *   * o _ *
+        // o o o o *
+        // _ * _ o o *
+        // _   _ * *
+        // _   *
+        Ins resumeFrom[] = {
+            Ins::DATA, Ins::TURN, Ins::NOOP, Ins::NOOP, Ins::DATA, Ins::TURN, Ins::DATA, Ins::TURN,
+            Ins::DATA, Ins::TURN, Ins::NOOP, Ins::NOOP, Ins::TURN, Ins::DATA, Ins::DATA, Ins::DATA,
+            Ins::TURN, Ins::NOOP, Ins::TURN, Ins::TURN, Ins::DATA, Ins::TURN, Ins::DATA, Ins::DATA,
+            Ins::TURN, Ins::UNSET
+        };
+        searcher.findOne(resumeFrom);
+
+        // TEMP: Should not yet be detected with current logic. Eventually it should be detected.
+        REQUIRE(tracker.getTotalDetectedHangs() == 0);
+    }
+    SECTION( "6x6-SweepWithInSequenceOscillatingZeros" ) {
+        // The body of the sweep consists of alternating -1 and 0 values. The left side is a fixed
+        // but increasing value, the right side of the sequence steadily grows. The leftward sweep
+        // inverts the entire sweep body. The -1 values become zeroes, and vice versa. Even though
+        // zero is an exit of the rightward sweep, this sweep moves DP two positions and is always
+        // aligned such that it uses the -1 values as stepping stones.
+        //
+        //       * *
+        //   * * o _ *
+        // * _ o o _ *
+        // * _ _ _ _ *
+        // * * o _ *
+        // o o o *
+        Ins resumeFrom[] = {
+            Ins::DATA, Ins::TURN, Ins::DATA, Ins::DATA, Ins::TURN, Ins::DATA, Ins::NOOP, Ins::DATA,
+            Ins::TURN, Ins::DATA, Ins::NOOP, Ins::TURN, Ins::NOOP, Ins::TURN, Ins::DATA, Ins::TURN,
+            Ins::TURN, Ins::NOOP, Ins::TURN, Ins::NOOP, Ins::NOOP, Ins::TURN, Ins::NOOP, Ins::TURN,
+            Ins::NOOP, Ins::TURN, Ins::TURN, Ins::TURN, Ins::UNSET
+        };
+        searcher.findOne(resumeFrom);
+
+        // TEMP: Should not yet be detected with current logic. Eventually it should be detected.
+        REQUIRE(tracker.getTotalDetectedHangs() == 0);
+    }
 }
