19edd94ec16462f6b81dfabf57f18f77e9e8ca50
Erwin Bonsma <eriban@users.sourceforge.net>
2023-08-21 15:17:24 +0200

    Add FastExecutor test

---
+++ Tests/FastExecutorTests.cpp
@@ -0,0 +1,37 @@
+//
+//  FastExecutorTests.cpp
+//  Tests
+//
+//  Created by Erwin on 15/08/2023.
+//  Copyright © 2023 Erwin. All rights reserved.
+//
+
+#include "catch.hpp"
+
+#include "FastExecutor.h"
+#include "InterpretedProgramBuilder.h"
+#include "Program.h"
+
+TEST_CASE("6x6 Fast Executor tests", "[6x6][fast-exec]") {
+    FastExecutor fastExecutor(1024);
+
+    SECTION("6x6-FastExecution-DataError") {
+        // Program that results in a DATA_ERROR. It erroneously resulted in a PROGRAM_ERROR in an
+        // earlier version.
+        //
+        //   * *   *
+        // * _ o _ o *
+        // o _ _ * *
+        // _ * _ _ _
+        // _ * o _ _ *
+        // _   *
+        Program program = Program::fromString("66hfseixf_gbb_");
+        InterpretedProgramBuilder programBuilder =
+            InterpretedProgramBuilder::fromProgram(program);
+
+        // Act
+        RunResult result = fastExecutor.execute(&programBuilder);
+
+        REQUIRE(result == RunResult::DATA_ERROR);
+    }
+}
