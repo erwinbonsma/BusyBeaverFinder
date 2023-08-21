e80883085ecf2c41b7a43c2ca6723bb64267d1d9
Erwin Bonsma <eriban@users.sourceforge.net>
2023-08-21 21:02:21 +0200

    Convert glider hang tests

--- Tests/GliderHangTests.cpp
+++ Tests/GliderHangTests.cpp
@@ -6,24 +6,15 @@
 //  Copyright © 2019 Erwin Bonsma.
 //
 
-#include <stdio.h>
-
 #include "catch.hpp"
 
-#include "ExhaustiveSearcher.h"
+#include "HangExecutor.h"
 
-TEST_CASE( "6x6 Glider Hang tests", "[hang][glider][6x6]" ) {
-    SearchSettings settings = defaultSearchSettings;
-    settings.maxHangDetectionSteps = 1000000;
-    settings.maxSteps = settings.maxHangDetectionSteps;
-    settings.disableNoExitHangDetection = true;
+TEST_CASE("6x6 Glider Hang tests", "[hang][glider][6x6]") {
+    HangExecutor hangExecutor(1024, 1000000);
+    hangExecutor.setMaxSteps(1000000);
 
-    ExhaustiveSearcher searcher(6, 6, settings);
-    ProgressTracker tracker(searcher);
-
-    searcher.setProgressTracker(&tracker);
-
-    SECTION( "6x6-Glider1") {
+    SECTION("6x6-Glider1") {
         // A non-periodic hang where two ever-increasing values move rightward on the tape, leaving
         // zeroes in their wake. The amount of steps required to move one position on the tape
         // doubles each time (as the left counter is decreased towards zero, the right counter is
@@ -35,17 +26,12 @@ TEST_CASE( "6x6 Glider Hang tests", "[hang][glider][6x6]" ) {
         //   _ * o o
         // * _ o o *
         // o o * *
-        Ins resumeFrom[] = {
-            Ins::DATA, Ins::TURN, Ins::DATA, Ins::TURN, Ins::NOOP, Ins::NOOP, Ins::DATA, Ins::TURN,
-            Ins::NOOP, Ins::DATA, Ins::DATA, Ins::TURN, Ins::DATA, Ins::TURN, Ins::TURN, Ins::DATA,
-            Ins::TURN, Ins::DATA, Ins::TURN, Ins::DATA, Ins::TURN, Ins::DATA, Ins::NOOP, Ins::TURN,
-            Ins::NOOP, Ins::UNSET
-        };
-        searcher.findOne(resumeFrom);
+        RunResult result = hangExecutor.execute("66_xfecnblsonr");
 
-        REQUIRE(tracker.getTotalHangs(HangType::APERIODIC_GLIDER) == 1);
+        REQUIRE(result == RunResult::DETECTED_HANG);
+        REQUIRE(hangExecutor.detectedHangType() == HangType::APERIODIC_GLIDER);
     }
-    SECTION( "6x6-Glider2" ) {
+    SECTION("6x6-Glider2") {
         // A glider that moves leftwards.
         //
         //     *
@@ -54,16 +40,12 @@ TEST_CASE( "6x6 Glider Hang tests", "[hang][glider][6x6]" ) {
         // o * o o _ *
         // o   _ *
         // o   *
-        Ins resumeFrom[] = {
-            Ins::DATA, Ins::DATA, Ins::DATA, Ins::DATA, Ins::TURN, Ins::NOOP, Ins::NOOP, Ins::NOOP,
-            Ins::TURN, Ins::DATA, Ins::TURN, Ins::DATA, Ins::TURN, Ins::NOOP, Ins::TURN, Ins::DATA,
-            Ins::TURN, Ins::DATA, Ins::TURN, Ins::NOOP, Ins::TURN, Ins::UNSET
-        };
-        searcher.findOne(resumeFrom);
+        RunResult result = hangExecutor.execute("66b_soifpkirk_");
 
-        REQUIRE(tracker.getTotalHangs(HangType::APERIODIC_GLIDER) == 1);
+        REQUIRE(result == RunResult::DETECTED_HANG);
+        REQUIRE(hangExecutor.detectedHangType() == HangType::APERIODIC_GLIDER);
     }
-    SECTION( "6x6-Glider3" ) {
+    SECTION("6x6-Glider3") {
         // Another leftward moving glider.
         //
         //     *   *
@@ -72,17 +54,12 @@ TEST_CASE( "6x6 Glider Hang tests", "[hang][glider][6x6]" ) {
         // o _ o * _
         // o * _ o o *
         // _   *   *
-        Ins resumeFrom[] = {
-            Ins::NOOP, Ins::DATA, Ins::DATA, Ins::DATA, Ins::TURN, Ins::NOOP, Ins::NOOP, Ins::NOOP,
-            Ins::NOOP, Ins::TURN, Ins::NOOP, Ins::DATA, Ins::TURN, Ins::DATA, Ins::NOOP, Ins::TURN,
-            Ins::TURN, Ins::TURN, Ins::NOOP, Ins::TURN, Ins::DATA, Ins::DATA, Ins::DATA, Ins::NOOP,
-            Ins::DATA, Ins::TURN, Ins::TURN, Ins::TURN, Ins::UNSET
-        };
-        searcher.findOne(resumeFrom);
+        RunResult result = hangExecutor.execute("66bfvkibjronbf");
 
-        REQUIRE(tracker.getTotalHangs(HangType::APERIODIC_GLIDER) == 1);
+        REQUIRE(result == RunResult::DETECTED_HANG);
+        REQUIRE(hangExecutor.detectedHangType() == HangType::APERIODIC_GLIDER);
     }
-    SECTION( "6x6-Glider4" ) {
+    SECTION("6x6-Glider4") {
         // A glider that was wrongly found by an early version of the Regular Sweep Hang detector.
         //
         //     * *
@@ -91,16 +68,12 @@ TEST_CASE( "6x6 Glider Hang tests", "[hang][glider][6x6]" ) {
         // o * o o _ *
         // o * o *
         // o   *
-        Ins resumeFrom[] = {
-            Ins::DATA, Ins::DATA, Ins::DATA, Ins::DATA, Ins::TURN, Ins::NOOP, Ins::DATA, Ins::NOOP,
-            Ins::TURN, Ins::DATA, Ins::TURN, Ins::TURN, Ins::DATA, Ins::TURN, Ins::NOOP, Ins::TURN,
-            Ins::DATA, Ins::TURN, Ins::DATA, Ins::TURN, Ins::TURN, Ins::DATA, Ins::TURN, Ins::UNSET
-        };
-        searcher.findOne(resumeFrom);
+        RunResult result = hangExecutor.execute("66brsojfpkprk_");
 
-        REQUIRE(tracker.getTotalHangs(HangType::APERIODIC_GLIDER) == 1);
+        REQUIRE(result == RunResult::DETECTED_HANG);
+        REQUIRE(hangExecutor.detectedHangType() == HangType::APERIODIC_GLIDER);
     }
-    SECTION( "6x6-Glider5" ) {
+    SECTION("6x6-Glider5") {
         // A glider that was wrongly found by an early version of the Regular Sweep Hang detector.
         //
         //   *   *
@@ -109,17 +82,12 @@ TEST_CASE( "6x6 Glider Hang tests", "[hang][glider][6x6]" ) {
         // * _ o * _
         // * * * o o *
         // o _ _ o *
-        Ins resumeFrom[] = {
-            Ins::DATA, Ins::TURN, Ins::NOOP, Ins::NOOP, Ins::DATA, Ins::TURN, Ins::DATA, Ins::TURN,
-            Ins::DATA, Ins::TURN, Ins::NOOP, Ins::NOOP, Ins::TURN, Ins::DATA, Ins::NOOP, Ins::DATA,
-            Ins::TURN, Ins::DATA, Ins::TURN, Ins::DATA, Ins::NOOP, Ins::TURN, Ins::NOOP, Ins::TURN,
-            Ins::DATA, Ins::TURN, Ins::TURN, Ins::TURN, Ins::TURN, Ins::UNSET
-        };
-        searcher.findOne(resumeFrom);
+        RunResult result = hangExecutor.execute("66frvfuksrznio");
 
-        REQUIRE(tracker.getTotalHangs(HangType::APERIODIC_GLIDER) == 1);
+        REQUIRE(result == RunResult::DETECTED_HANG);
+        REQUIRE(hangExecutor.detectedHangType() == HangType::APERIODIC_GLIDER);
     }
-    SECTION( "6x6-Glider6" ) {
+    SECTION("6x6-Glider6") {
         // Glider Hang that is challenging to detect as the meta-run loop starts relatively late and
         // the iterations of glider loop increases exponentially. The program therefore runs for
         // many steps (~100000) before the hang is detected.
@@ -130,17 +98,12 @@ TEST_CASE( "6x6 Glider Hang tests", "[hang][glider][6x6]" ) {
         // o * _   o
         // _ * _ o o *
         // _   *   *
-        Ins resumeFrom[] = {
-            Ins::NOOP, Ins::NOOP, Ins::DATA, Ins::DATA, Ins::TURN, Ins::NOOP, Ins::NOOP, Ins::DATA,
-            Ins::DATA, Ins::TURN, Ins::NOOP, Ins::TURN, Ins::DATA, Ins::DATA, Ins::DATA, Ins::TURN,
-            Ins::DATA, Ins::DATA, Ins::TURN, Ins::TURN, Ins::TURN, Ins::DATA, Ins::NOOP, Ins::TURN,
-            Ins::TURN, Ins::NOOP, Ins::TURN, Ins::UNSET
-        };
-        searcher.findOne(resumeFrom);
+        RunResult result = hangExecutor.execute("66bfvkinocfnbf");
 
-        REQUIRE(tracker.getTotalHangs(HangType::APERIODIC_GLIDER) == 1);
+        REQUIRE(result == RunResult::DETECTED_HANG);
+        REQUIRE(hangExecutor.detectedHangType() == HangType::APERIODIC_GLIDER);
     }
-    SECTION( "6x6-Glider7" ) {
+    SECTION("6x6-Glider7") {
         // Another leftward moving glider, but with relatively complex logic
         //
         //   * *   *
@@ -149,17 +112,12 @@ TEST_CASE( "6x6 Glider Hang tests", "[hang][glider][6x6]" ) {
         // o _ o * o
         // o * _ _ o *
         // o   *   *
-        Ins resumeFrom[] = {
-            Ins::DATA, Ins::DATA, Ins::DATA, Ins::DATA, Ins::TURN, Ins::NOOP, Ins::NOOP, Ins::TURN,
-            Ins::DATA, Ins::NOOP, Ins::TURN, Ins::TURN, Ins::DATA, Ins::TURN, Ins::DATA, Ins::NOOP,
-            Ins::TURN, Ins::TURN, Ins::DATA, Ins::NOOP, Ins::TURN, Ins::DATA, Ins::DATA, Ins::DATA,
-            Ins::TURN, Ins::NOOP, Ins::TURN, Ins::TURN, Ins::UNSET
-        };
-        searcher.findOne(resumeFrom);
+        RunResult result = hangExecutor.execute("66hfvkiujuoekf");
 
-        REQUIRE(tracker.getTotalHangs(HangType::APERIODIC_GLIDER) == 1);
+        REQUIRE(result == RunResult::DETECTED_HANG);
+        REQUIRE(hangExecutor.detectedHangType() == HangType::APERIODIC_GLIDER);
     }
-    SECTION( "6x6-Glider8" ) {
+    SECTION("6x6-Glider8") {
         // A glider hang that is detected, but only very late (after 71140 steps). The reason it
         // takes this long is that the length of each loop triples each iteration and the detector
         // conservatively requires a few meta-loop iterations before concluding its a hang.
@@ -170,17 +128,12 @@ TEST_CASE( "6x6 Glider Hang tests", "[hang][glider][6x6]" ) {
         // o _ o * _
         // o * _ o o *
         // o   *   *
-        Ins resumeFrom[] = {
-            Ins::DATA, Ins::DATA, Ins::DATA, Ins::DATA, Ins::TURN, Ins::NOOP, Ins::DATA, Ins::DATA,
-            Ins::NOOP, Ins::TURN, Ins::NOOP, Ins::TURN, Ins::DATA, Ins::DATA, Ins::DATA, Ins::NOOP,
-            Ins::TURN, Ins::DATA, Ins::TURN, Ins::NOOP, Ins::TURN, Ins::TURN, Ins::TURN, Ins::NOOP,
-            Ins::DATA, Ins::TURN, Ins::DATA, Ins::TURN, Ins::UNSET
-        };
-        searcher.findOne(resumeFrom);
+        RunResult result = hangExecutor.execute("66bfvkjkjronkf");
 
-        REQUIRE(tracker.getTotalHangs(HangType::APERIODIC_GLIDER) == 1);
+        REQUIRE(result == RunResult::DETECTED_HANG);
+        REQUIRE(hangExecutor.detectedHangType() == HangType::APERIODIC_GLIDER);
     }
-    SECTION( "6x6-Glider9" ) {
+    SECTION("6x6-Glider9") {
         // Leftward moving glider which in its transition sequence modifies a datacell in its wake.
         // It was not detected by the initial implementaton of the static Glider Hang detector,
         // which did not support these changes yet.
@@ -191,17 +144,12 @@ TEST_CASE( "6x6 Glider Hang tests", "[hang][glider][6x6]" ) {
         //   _ * _ *
         // * *   _
         // o _ o o *
-        Ins resumeFrom[] = {
-            Ins::DATA, Ins::TURN, Ins::NOOP, Ins::DATA, Ins::DATA, Ins::TURN, Ins::NOOP, Ins::NOOP,
-            Ins::DATA, Ins::TURN, Ins::NOOP, Ins::TURN, Ins::TURN, Ins::DATA, Ins::DATA, Ins::TURN,
-            Ins::NOOP, Ins::TURN, Ins::TURN, Ins::DATA, Ins::TURN, Ins::DATA, Ins::DATA, Ins::TURN,
-            Ins::TURN, Ins::UNSET
-        };
-        searcher.findOne(resumeFrom);
+        RunResult result = hangExecutor.execute("66ffdwvkbfx_jo");
 
-        REQUIRE(tracker.getTotalHangs(HangType::APERIODIC_GLIDER) == 1);
+        REQUIRE(result == RunResult::DETECTED_HANG);
+        REQUIRE(hangExecutor.detectedHangType() == HangType::APERIODIC_GLIDER);
     }
-    SECTION( "6x6-Glider10" ) {
+    SECTION("6x6-Glider10") {
         // A glider hang where the transition sequence consists of a fixed loop that always loops
         // twice.
         //
@@ -211,37 +159,12 @@ TEST_CASE( "6x6 Glider Hang tests", "[hang][glider][6x6]" ) {
         // * _ _ o *
         // * * _ *
         // o _ o *
-        Ins resumeFrom[] = {
-            Ins::DATA, Ins::TURN, Ins::NOOP, Ins::DATA, Ins::TURN, Ins::NOOP, Ins::NOOP, Ins::DATA,
-            Ins::DATA, Ins::TURN, Ins::DATA, Ins::NOOP, Ins::TURN, Ins::TURN, Ins::DATA, Ins::TURN,
-            Ins::DATA, Ins::NOOP, Ins::TURN, Ins::TURN, Ins::TURN, Ins::DATA, Ins::TURN, Ins::DATA,
-            Ins::TURN, Ins::DATA, Ins::TURN, Ins::TURN, Ins::UNSET
-        };
-        searcher.findOne(resumeFrom);
+        RunResult result = hangExecutor.execute("66hfvkvnroxrjr");
 
-        REQUIRE(tracker.getTotalHangs(HangType::APERIODIC_GLIDER) == 1);
+        REQUIRE(result == RunResult::DETECTED_HANG);
+        REQUIRE(hangExecutor.detectedHangType() == HangType::APERIODIC_GLIDER);
     }
-    SECTION( "6x6-Glider11" ) {
-        // A glider hang where the transition sequence consists of a fixed loop that always loops
-        // twice. It was first detected by the static hang detector.
-        //
-        //   * *   *
-        // * o o o _ *
-        // * o o o o *
-        // * _ _ o *
-        // * * _ *
-        // o _ o *
-        Ins resumeFrom[] = {
-            Ins::DATA, Ins::TURN, Ins::NOOP, Ins::DATA, Ins::TURN, Ins::NOOP, Ins::NOOP, Ins::DATA,
-            Ins::DATA, Ins::TURN, Ins::DATA, Ins::NOOP, Ins::TURN, Ins::TURN, Ins::DATA, Ins::TURN,
-            Ins::DATA, Ins::NOOP, Ins::TURN, Ins::TURN, Ins::TURN, Ins::DATA, Ins::TURN, Ins::DATA,
-            Ins::TURN, Ins::DATA, Ins::TURN, Ins::TURN, Ins::UNSET
-        };
-        searcher.findOne(resumeFrom);
-
-        REQUIRE(tracker.getTotalHangs(HangType::APERIODIC_GLIDER) == 1);
-    }
-    SECTION( "6x6-Glider12" ) {
+    SECTION("6x6-Glider12") {
         // A glider hang where the transition sequence modifies values further ahead than the
         // next-next loop counter. This caused the old "only zeros ahead" check to fail.
         //
@@ -251,17 +174,12 @@ TEST_CASE( "6x6 Glider Hang tests", "[hang][glider][6x6]" ) {
         // * _ o o *
         // * * _ _
         // o _ o *
-        Ins resumeFrom[] = {
-            Ins::DATA, Ins::TURN, Ins::NOOP, Ins::DATA, Ins::TURN, Ins::NOOP, Ins::DATA, Ins::DATA,
-            Ins::DATA, Ins::TURN, Ins::DATA, Ins::NOOP, Ins::TURN, Ins::TURN, Ins::DATA, Ins::TURN,
-            Ins::DATA, Ins::NOOP, Ins::TURN, Ins::TURN, Ins::TURN, Ins::DATA, Ins::TURN, Ins::DATA,
-            Ins::TURN, Ins::DATA, Ins::NOOP, Ins::TURN, Ins::UNSET
-        };
-        searcher.findOne(resumeFrom);
+        RunResult result = hangExecutor.execute("66hfvkvnsox_jr");
 
-        REQUIRE(tracker.getTotalHangs(HangType::APERIODIC_GLIDER) == 1);
+        REQUIRE(result == RunResult::DETECTED_HANG);
+        REQUIRE(hangExecutor.detectedHangType() == HangType::APERIODIC_GLIDER);
     }
-    SECTION( "6x6-Glider13" ) {
+    SECTION("6x6-Glider13") {
         // A glider similar in behavior to Glider12, but with a different, pretty program.
         //   * *   *
         // * o o o _ *
@@ -269,51 +187,35 @@ TEST_CASE( "6x6 Glider Hang tests", "[hang][glider][6x6]" ) {
         // * _ o   _
         // * * o _ o *
         // o _ o * *
-        Ins resumeFrom[] = {
-            Ins::DATA, Ins::TURN, Ins::NOOP, Ins::DATA, Ins::TURN, Ins::DATA, Ins::DATA, Ins::DATA,
-            Ins::DATA, Ins::TURN, Ins::DATA, Ins::NOOP, Ins::TURN, Ins::TURN, Ins::DATA, Ins::TURN,
-            Ins::DATA, Ins::NOOP, Ins::TURN, Ins::TURN, Ins::TURN, Ins::NOOP, Ins::NOOP, Ins::DATA,
-            Ins::TURN, Ins::NOOP, Ins::TURN, Ins::UNSET
-        };
-        searcher.findOne(resumeFrom);
+        RunResult result = hangExecutor.execute("66hfvkd_s_yejx");
 
-        REQUIRE(tracker.getTotalHangs(HangType::APERIODIC_GLIDER) == 1);
+        REQUIRE(result == RunResult::DETECTED_HANG);
+        REQUIRE(hangExecutor.detectedHangType() == HangType::APERIODIC_GLIDER);
     }
-    SECTION( "6x6-GliderWithCounterQueue" ) {
+    SECTION("6x6-GliderWithCounterQueue") {
         // Glider hang where the next counter in the glider loop is not immediately the current
         // counter in the next glider loop iteration. It takes two iterations of the glider loop
         // before it becomes the current counter. So effectively there is a queue of glider
         // counters.
         //
-        //       *
-        // *   * o _ *
-        // o o o o *
-        // _ * _ o o *
-        // _   _ * *
-        // _   *
-        Ins resumeFrom[] = {
-            Ins::DATA, Ins::TURN, Ins::NOOP, Ins::NOOP, Ins::DATA, Ins::TURN, Ins::DATA, Ins::TURN,
-            Ins::DATA, Ins::TURN, Ins::NOOP, Ins::NOOP, Ins::TURN, Ins::DATA, Ins::DATA, Ins::DATA,
-            Ins::TURN, Ins::NOOP, Ins::TURN, Ins::TURN, Ins::DATA, Ins::TURN, Ins::DATA, Ins::DATA,
-            Ins::TURN, Ins::UNSET
-        };
-        searcher.findOne(resumeFrom);
+        //   *
+        //   o o o *
+        // * o o o _ *
+        //   _ * *
+        // * *   o o *
+        // o _ _ o *
+        RunResult result = hangExecutor.execute("66f_dovkbrxnio");
 
-        REQUIRE(tracker.getTotalHangs(HangType::APERIODIC_GLIDER) == 1);
+        REQUIRE(result == RunResult::DETECTED_HANG);
+        REQUIRE(hangExecutor.detectedHangType() == HangType::APERIODIC_GLIDER);
     }
 }
 
-TEST_CASE( "7x7 Glider Hang tests", "[hang][glider][7x7]" ) {
-    SearchSettings settings = defaultSearchSettings;
-    settings.maxHangDetectionSteps = 1000000;
-    settings.maxSteps = settings.maxHangDetectionSteps;
+TEST_CASE("7x7 Glider Hang tests", "[hang][glider][7x7]") {
+    HangExecutor hangExecutor(1024, 1000000);
+    hangExecutor.setMaxSteps(1000000);
 
-    ExhaustiveSearcher searcher(7, 7, settings);
-    ProgressTracker tracker(searcher);
-
-    searcher.setProgressTracker(&tracker);
-
-    SECTION( "7x7-GliderWithLargeRunBlocks") {
+    SECTION("7x7-GliderWithLargeRunBlocks") {
         // After a fairly chaotic start (which creates various one-off run blocks that contain many
         // program blocks) this program ends up in a glider hang. The glider hang itself also
         // consists of two large run blocks
@@ -344,15 +246,9 @@ TEST_CASE( "7x7 Glider Hang tests", "[hang][glider][7x7]" ) {
         //   _ _ * o
         // * _ o * _ *
         // o o *   *
-        Ins resumeFrom[] = {
-            Ins::DATA, Ins::TURN, Ins::DATA, Ins::TURN, Ins::NOOP, Ins::NOOP, Ins::DATA, Ins::TURN,
-            Ins::DATA, Ins::DATA, Ins::NOOP, Ins::TURN, Ins::DATA, Ins::TURN, Ins::DATA, Ins::TURN,
-            Ins::TURN, Ins::DATA, Ins::NOOP, Ins::NOOP, Ins::DATA, Ins::DATA, Ins::TURN, Ins::NOOP,
-            Ins::TURN, Ins::NOOP, Ins::TURN, Ins::TURN, Ins::DATA, Ins::NOOP, Ins::TURN, Ins::TURN,
-            Ins::TURN, Ins::UNSET
-        };
-        searcher.findOne(resumeFrom);
+        RunResult result = hangExecutor.execute("77b_fkrsnvk_gbefnf_");
 
-        REQUIRE(tracker.getTotalDetectedHangs() == 1);
+        REQUIRE(result == RunResult::DETECTED_HANG);
+        REQUIRE(hangExecutor.detectedHangType() == HangType::APERIODIC_GLIDER);
     }
 }
