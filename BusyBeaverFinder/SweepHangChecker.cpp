dd77a11f5c792f5f6954411f4e42214c8c7ce1ce
Erwin Bonsma <eriban@users.sourceforge.net>
2023-12-16 11:39:13 +0100

    Add SweepHangChecker files

---
+++ BusyBeaverFinder/SweepHangChecker.cpp
@@ -0,0 +1,9 @@
+//
+//  SweepHangChecker.cpp
+//  BusyBeaverFinder
+//
+//  Created by Erwin on 16/12/2023.
+//  Copyright © 2023 Erwin. All rights reserved.
+//
+
+#include "SweepHangChecker.h"
---
+++ BusyBeaverFinder/SweepHangChecker.h
@@ -0,0 +1,9 @@
+//
+//  SweepHangChecker.h
+//  BusyBeaverFinder
+//
+//  Created by Erwin on 16/12/2023.
+//  Copyright © 2023 Erwin. All rights reserved.
+//
+#pragma once
+
